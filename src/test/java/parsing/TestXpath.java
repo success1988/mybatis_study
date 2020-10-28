e3773d51ef8e9579e503ee9e9931367c3cdc7ef6
wangchenggong200506 <wangchenggong200506@credithc.com>
2020-10-28 19:11:07 +0800

    获取类路径，解析xml

--- src/test/java/parsing/TestXpath.java
+++ src/test/java/parsing/TestXpath.java
@@ -7,7 +7,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
-
 /**
  * @Title：
  * @Author：wangchenggong
@@ -18,18 +17,29 @@ import javax.xml.xpath.XPathFactory;
 public class TestXpath {
 
     public static void main(String[] args) throws Exception {
-        String resource = "mybatis.xml";
+        String resource = "info2.xml";
+
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-//        Document document = documentBuilder.parse(Thread.currentThread().getContextClassLoader().getResourceAsStream(resource));
-        Document document = documentBuilder.parse(TestXpath.class.getResourceAsStream(resource));
+
+        //如何读取
+        //方式一：利用Class的getResourceAsStream
+        //注意： TestXpath.class.getResourceAsStream("aaa.xml")取的是 TestXpath.class文件的相对路径，即TestXpath.class文件同级目录下的aaa.xml文件
+        //      TestXpath.class.getResourceAsStream("/aaa.xml")取的是 类路径（classes目录）下的aaa.xml文件
+        Document document = documentBuilder.parse(TestXpath.class.getResourceAsStream("/"+resource));
+
+
+
+
+
+        //Document document = documentBuilder.parse(Thread.currentThread().getContextClassLoader().getResourceAsStream(resource));
+        //Document document = documentBuilder.parse(TestXpath.class.getResourceAsStream(resource));
 
         XPathFactory xPathFactory = XPathFactory.newInstance();
         XPath xPath = xPathFactory.newXPath();
 
-        XPathExpression xPathExpression = xPath.compile("mappers/*[1]");
+        XPathExpression xPathExpression = xPath.compile("/members/user[id=1]");
         System.out.println(xPathExpression.evaluate(document));
-
 
     }
 
